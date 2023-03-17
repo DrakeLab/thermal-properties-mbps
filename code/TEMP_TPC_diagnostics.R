4bfbeb314efb5710ccc100648d71f6b0a5040c26
Kyle Dahlin <kydahlin@gmail.com>
2023-03-17 18:05:09 -0400

    remove attempt at parallelization

--- code/TEMP_TPC_diagnostics.R
+++ code/TEMP_TPC_diagnostics.R
@@ -46,26 +46,6 @@ for (sample_num in 1:dim(distinct_combos)[1]) {
   samples <- rbind(samples, temp_sample)
 }
 
-distinct_combos <- distinct(data_in, trait.name, system_ID)
-
-library(foreach)
-library(doParallel)
-cores <- detectCores()
-cl <- makeCluster(cores[1] - 1, outfile = "") # not to overload your computer
-registerDoParallel(cl)
-
-library(progressr)
-handlers(global = TRUE)
-
-samples <- tibble(
-  trait = as.character(),
-  system_ID = as.character(),
-  T0 = as.double(),
-  Tm = as.double(),
-  c = as.double(), # !!! note that we're using c as a generic parameter for Briere or Quadratic
-  sample_num = as.double(),
-  func = as.character()
-)
 
 # Save samples for now
 write_csv(samples, "data/clean/temp_samples.csv")
