ee04418e93d929006fb68412a495baf2da80a450
Kyle Dahlin <kydahlin@gmail.com>
2023-05-18 16:18:36 -0400

    Update output-functions.R

--- code/output-functions.R
+++ code/output-functions.R
@@ -47,19 +47,6 @@ compute.V0 <- function(input) {
   })
 }
 
-# compute.V0 <- function(input) {
-#     eps <- .Machine$double.eps
-#     # if(is.null(lf)) {lf = 1 / (muV + eps)}
-#     temp <- input$sigmaV_f * input$deltaL
-#     # check if net reproduction rate exceeds mortality rate
-#     temp_bool <- temp > (1 / input$lf)
-#     V0 <- ifelse(temp_bool,
-#            input$KL * input$rhoL * input$lf * (1 - 1 / (input$lf * temp + eps)) ,
-#            0 # if mortality exceeds reproduction, set to zero
-#     )
-#     return(V0)
-# }
-
 ### Host-to-mosquito reproduction number----------------------------------------
 compute.RH <- function(input) {
   with(input, {
